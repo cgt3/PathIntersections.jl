d5ed37e46729f292afdf7e3bc4b8aa62b8ccd719
Christina <cgt3@rice.edu>
2022-05-06 20:24:56 -0500

    Added test for curve outside domain

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,6 @@ using Revise
 
 @testset "PathIntersections.jl" begin
     # Write your tests here.
-    intersection = Intersection(0, [1,1], [1,0], [2,3])
-    @test intersection.s == 0
     
     x_coords = LinRange(-1,1,3)
     y_coords = LinRange(-1,1,3)
@@ -28,8 +26,8 @@ using Revise
     # 2. Simple, single-dim intersections with start/endpoint on a boundary
     # Note: start and end point are both counted, so their common intersection
     # gets counted twice
-    circleParam2 = (; r=0.5, x0=0, y0=0)
-    intersections = find_mesh_intersections_single(coords, circle, ds, arc_tol, single_tol, circleParam2 )
+    circleParam = (; r=0.5, x0=0, y0=0)
+    intersections = find_mesh_intersections_single(coords, circle, ds, arc_tol, single_tol, circleParam )
     @test length(intersections) == 5
     @test intersections.s[1] == 0
     @test intersections.s[2] == 0.25
@@ -41,11 +39,10 @@ using Revise
     epsilon = 0.001
     x_coords = [-1, 0, 0.5*sqrt(2)/2-epsilon, 1]
     y_coords = [-1, 0, 0.5*sqrt(2)/2-epsilon, 1]
-    ds = 1/100 # = 1deg
     coords = [x_coords, y_coords]
-    circleParams3 = (; r=0.5, x0=0, y0=0)
+    circleParams = (; r=0.5, x0=0, y0=0)
     
-    intersections = find_mesh_intersections_single(coords, circle, ds, arc_tol, single_tol, circleParam2 )
+    intersections = find_mesh_intersections_single(coords, circle, ds, arc_tol, single_tol, circleParam )
     @test length(intersections) == 9
     @test intersections.s[1] == 0
     @test intersections.s[4] == 0.25
@@ -57,11 +54,10 @@ using Revise
     # 4. Corners, x first, then y
     x_coords = [-1, 0, 0.5*sqrt(2)/2, 1]
     y_coords = [-1, 0, 0.5*sqrt(2)/2, 1]
-    ds = 1/100 # = 1deg
     coords = [x_coords, y_coords]
-    circleParams3 = (; r=0.5, x0=0, y0=0)
+    circleParams = (; r=0.5, x0=0, y0=0)
     
-    intersections = find_mesh_intersections_single(coords, circle, ds, arc_tol, single_tol, circleParam2 )
+    intersections = find_mesh_intersections_single(coords, circle, ds, arc_tol, single_tol, circleParam )
     @test length(intersections) == 8
     @test intersections.s[1] == 0
     @test intersections.s[2] == 0.125
@@ -71,11 +67,15 @@ using Revise
     @test intersections.s[6] == 0.75
     @test intersections.s[7] == 0.875
     @test intersections.s[8] == 1.0
-    
-
-    # 5. Corners, y first, then x 
 
     # 6. Curve is completely outside the domain
+    x_coords = LinRange(-1,1,3)
+    y_coords = LinRange(-1,1,3)
+    coords = [x_coords, y_coords]
+    circleParam = (; r=0.5, x0=2, y0=2)
+    
+    intersections = find_mesh_intersections_single(coords, circle, ds, arc_tol, single_tol, circleParam )
+    @test length(intersections) == 0
 
     # 7. Curve that starts outside the domain then enters
 
